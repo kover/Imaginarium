b00d57fea4ebf71582752fe8d0588c00d0dedfc9
Konstantin Penzin <koverboard@icloud.com>
2023-06-08 23:09:02 +0500

    Profile view layout

---
+++ Imaginarium/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Imaginarium
+//
+//  Created by Konstantin Penzin on 08.06.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var profilePictureImageView: UIImageView!
+    @IBOutlet private var fullNameLabel: UILabel!
+    @IBOutlet private var userNameLabel: UILabel!
+    @IBOutlet private var profileDescriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func logoutTap() {
+        
+    }
+}
